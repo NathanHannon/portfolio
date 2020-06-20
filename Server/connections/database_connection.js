68922932ca97a487d3dcb1e2ef4d3479f6fd5194
Nathan Hannon <nathan@hannon.me>
2020-06-20 19:19:42 -0400

    fixed db connection

--- Server/connections/database_connection.js
+++ Server/connections/database_connection.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 const Pool = require('pg').Pool;
 const pool = new Pool({
 	user: 'postgres',
-	host: '127.0.0.1',
+	host: '35.231.19.245',
 	database: 'PortfolioDB',
 	password: 'Nhannon03231996!?><',
-	port: 5433
+	port: 5432
 });
 
 
