06ab2f5513cdf5ad9feda19f5c3ebd90a2ead31b
Nathan Hannon <nathan@hannon.me>
2020-06-20 18:47:24 -0400

    updated imports

--- Server/connections/database_connection.js
+++ Server/connections/database_connection.js
@@ -2,10 +2,20 @@ require('dotenv').config();
 const Pool = require('pg').Pool;
 const pool = new Pool({
 	user: 'postgres',
-	host: '35.231.19.245',
+	host: '127.0.0.1',
 	database: 'PortfolioDB',
 	password: 'Nhannon03231996!?><',
-	port: 5432
+	port: 5433
 });
 
+
+// require('dotenv').config();
+// const Pool = require('pg').Pool;
+// const pool = new Pool({
+// 	user: 'doadmin',
+// 	host: 'portfolio-db-do-user-6898555-0.a.db.ondigitalocean.com',
+// 	database: 'PortfolioDB',
+// 	password: 'u9pypkkxa5w328mv',
+// 	port: 25060
+// });
 module.exports = pool;
