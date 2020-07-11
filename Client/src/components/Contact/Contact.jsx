39176102b31b0fb910c85425fa368d9463fd7091
Nathan Hannon <nathan@hannon.me>
2020-07-10 21:58:53 -0400

    Added mobile friendly mailto and tel

--- Client/src/components/Contact/Contact.jsx
+++ Client/src/components/Contact/Contact.jsx
@@ -13,7 +13,7 @@ class Contact extends Component {
                     <a href="tel:519-878-6227" className="contactLink" alt="Call 519-878-6227">Give me a call!</a>
                     <a href="mailto:nathan@hannon.me" className="contactLink" alt="Send email to nathan@hannon.me">Send me an email!</a>
                 </p>
-                <iframe width="100%" height="1150" frameborder="0" src={`https://storage.cloud.google.com/portfoliofiles/Nathan_Hannon_Resume.pdf`}/>
+                <iframe width="100%" height="1150px" frameborder="0" src={`https://storage.cloud.google.com/portfoliofiles/Nathan_Hannon_Resume.pdf`}/>
              </Container>
         );
     }
