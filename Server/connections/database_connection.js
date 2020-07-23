55718987c8f7e12fe511f883ab3d623093d182b2
Nathan Hannon <nathan@hannon.me>
2020-07-23 12:24:37 -0400

    removed comment

--- Server/connections/database_connection.js
+++ Server/connections/database_connection.js
@@ -8,14 +8,4 @@ const pool = new Pool({
 	port: 5432
 });
 
-
-// require('dotenv').config();
-// const Pool = require('pg').Pool;
-// const pool = new Pool({
-// 	user: 'doadmin',
-// 	host: 'portfolio-db-do-user-6898555-0.a.db.ondigitalocean.com',
-// 	database: 'PortfolioDB',
-// 	password: 'u9pypkkxa5w328mv',
-// 	port: 25060
-// });
 module.exports = pool;
