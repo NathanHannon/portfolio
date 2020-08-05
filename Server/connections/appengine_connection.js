4531254d9712670900e57ad0a4166247f63a8315
Nathan Hannon <nathan@hannon.me>
2020-08-05 11:21:02 -0400

    updated strings

--- Server/connections/appengine_connection.js
+++ Server/connections/appengine_connection.js
@@ -1,15 +1,15 @@
 const connectWithUnixSockets = (config) => {
-    const dbSocketPath = process.env.DB_SOCKET_PATH || "/cloudsql"
-  
+    const instanceName = "portfolio-285502:us-east1:portfolio-db"
+    const dbSocketPath = process.env.DB_SOCKET_PATH || "/cloudsql/portfolio-285502:us-east1:portfolio-db/.s.PGSQL.5432"
+
     // Establish a connection to the database
     return Knex({
       client: 'pg',
       connection: {
         user: 'postgres',
-        host: '34.73.186.70',
-        database: 'PortfolioDB',
         password: 'Nhannon03231996!?><',
-        port: 5432
+        database: 'PortfolioDB',
+        host: `${dbSocketPath}/${instanceName}`
       },
       // ... Specify additional properties here.
       ...config
