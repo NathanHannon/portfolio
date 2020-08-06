5278dbf89e8b827246402f7a329c63c9619fac20
Nathan Hannon <nathan@hannon.me>
2020-08-05 23:43:40 -0400

    added instance name

--- Server/connections/appengine_connection.js
+++ Server/connections/appengine_connection.js
@@ -1,5 +1,5 @@
 const connectWithUnixSockets = (config) => {
-  const dbSocketPath = process.env.DB_SOCKET_PATH || "/cloudsql"
+  const dbSocketPath = process.env.DB_SOCKET_PATH || "/cloudsql/portfolio-285502:us-east1:portfolio-db"
 
   // Establish a connection to the database
   return Knex({
