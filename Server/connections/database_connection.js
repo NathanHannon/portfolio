f65038ef8b41075060752faed8e8faebaaf97b42
Nathan Hannon <nathan@hannon.me>
2020-08-06 10:13:51 -0400

    added tcp flexible connection

--- Server/connections/database_connection.js
+++ Server/connections/database_connection.js
@@ -2,7 +2,7 @@ require('../../node_modules/dotenv').config();
 const Pool = require('../../node_modules/pg').Pool;
 const pool = new Pool({
 	user: 'postgres',
-	host: '10.113.208.3',
+	host: '35.229.62.112',
 	database: 'PortfolioDB',
 	password: 'Nhannon03231996!?><',
 	port: 5432
