ed93ccb9dcca09ad2d846cb64b9231341701127b
Nathan Hannon <nathan@hannon.me>
2020-08-20 18:42:36 -0400

    chnaged connection for digitalocean

--- server/connections/database_connection.js
+++ server/connections/database_connection.js
@@ -1,12 +1,26 @@
+const { findSeries } = require('async');
+
 // STANDARD/PLACEHOLDER CONNECTION
 require('../../node_modules/dotenv').config();
 const Pool = require('../../node_modules/pg').Pool;
 const pool = new Pool({
-	user: 'postgres',
-	host: '35.185.7.181',
+	user: 'doadmin',
+	password: 'lkon3ablk9qtndmv',
+	host: 'portfolio-cluster-do-user-6898555-0.a.db.ondigitalocean.com',
+	port: 25060,
 	database: 'PortfolioDB',
-	password: 'Nhannon03231996!?><',
-	port: 5432
+	ssl: {
+		require,
+		ca: '../ssl/do-ca-certificate.crt',
+		rejectUnauthorized: false,
+	} 
+	
+	
+	// user: 'postgres',
+	// host: '35.185.7.181',
+	// database: 'PortfolioDB',
+	// password: 'Nhannon03231996!?><',
+	// port: 5432
 });
 
 module.exports = pool;
