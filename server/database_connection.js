3fe4203e85c881891ee96e1a68e0be8f74b7ee1a
Nathan Hannon <nathan@hannon.me>
2020-09-16 16:57:07 -0400

    fixed missing pool issue

--- server/database_connection.js
+++ server/database_connection.js
@@ -1,13 +1,12 @@
 const { findSeries } = require('async');
 
+const Pool = require('pg/lib').Pool;
 const pool = new Pool({
 	connectionString: process.env.DATABASE_URL,
 	ssl: {
 		rejectUnauthorized: false
 	}
 });
-
-// const Pool = require('pg/lib').Pool;
 // const pool = new Pool({
 // 	host: 'app-afd58367-fde3-4647-88a5-0bac6dcd1e6a-do-user-6898555-0.b.db.ondigitalocean.com',
 // 	port: 25060,
