961e4a8c627286711c0aad5f17bb42f8a7c2ca0f
Nathan Hannon <nathan@hannon.me>
2020-09-16 16:10:24 -0400

    updated credentials

--- server/database_connection.js
+++ server/database_connection.js
@@ -5,22 +5,10 @@ const Pool = require('pg/lib').Pool;
 const pool = new Pool({
 	host: 'app-afd58367-fde3-4647-88a5-0bac6dcd1e6a-do-user-6898555-0.b.db.ondigitalocean.com',
 	port: 25060,
-	username: 'user',
+	user: 'portfolio-db',
 	password: 'kii9exh32ii6in4c',
 	database: 'portfolio-db',
 	sslmode: 'require'
-	// sslmode: {
-	// 	require,
-	// 	ca: '../ssl/do-ca-certificate.crt',
-	// 	rejectUnauthorized: false,
-	// },
-
-
-	// user: 'postgres',
-	// host: '35.185.7.181',
-	// database: 'PortfolioDB',
-	// password: 'Nhannon03231996!?><',
-	// port: 5432
 });
 
 module.exports = pool;
