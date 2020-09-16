6c4e98e310df3e0cdeb75e4815efe70b534d75c7
Nathan Hannon <nathan@hannon.me>
2020-09-16 15:05:36 -0400

    set default route to /api/

--- server/app.js
+++ server/app.js
@@ -21,6 +21,6 @@ app.use(express.static(path.join(__dirname, "public")));
 // app.listen(3000);
 
 //use create route api
-app.use("/", projectRouter);
+app.use("/api/", projectRouter);
 
 module.exports = app;
