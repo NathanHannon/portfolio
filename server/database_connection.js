e6825d68dd8b6d5ba4e4e49757585a30f78cfc96
Nathan Hannon <nathan@hannon.me>
2020-09-16 16:45:24 -0400

    moved from pool to connectionstring

--- server/database_connection.js
+++ server/database_connection.js
@@ -1,14 +1,20 @@
 const { findSeries } = require('async');
 
-// STANDARD/PLACEHOLDER CONNECTION
-const Pool = require('pg/lib').Pool;
 const pool = new Pool({
-	host: 'app-afd58367-fde3-4647-88a5-0bac6dcd1e6a-do-user-6898555-0.b.db.ondigitalocean.com',
-	port: 25060,
-	user: 'portfolio-db',
-	password: 'kii9exh32ii6in4c',
-	database: 'portfolio-db',
-	ssl: true
+	connectionString: process.env.DATABASE_URL,
+	ssl: {
+		rejectUnauthorized: false
+	}
 });
+
+// const Pool = require('pg/lib').Pool;
+// const pool = new Pool({
+// 	host: 'app-afd58367-fde3-4647-88a5-0bac6dcd1e6a-do-user-6898555-0.b.db.ondigitalocean.com',
+// 	port: 25060,
+// 	user: 'portfolio-db',
+// 	password: 'kii9exh32ii6in4c',
+// 	database: 'portfolio-db',
+// 	ssl: true
+// });
 
 module.exports = pool;
