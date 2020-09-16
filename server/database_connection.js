05345821bc9d0a426270c721a87bee79ceceb44e
Nathan Hannon <nathan@hannon.me>
2020-09-16 16:00:54 -0400

    changed db username

--- server/database_connection.js
+++ server/database_connection.js
@@ -5,7 +5,7 @@ const Pool = require('pg/lib').Pool;
 const pool = new Pool({
 	host: 'app-afd58367-fde3-4647-88a5-0bac6dcd1e6a-do-user-6898555-0.b.db.ondigitalocean.com',
 	port: 25060,
-	username: 'portfolio-db',
+	username: 'user',
 	password: 'kii9exh32ii6in4c',
 	database: 'portfolio-db',
 	sslmode: 'require'
