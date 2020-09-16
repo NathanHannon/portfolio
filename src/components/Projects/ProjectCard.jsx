908513e298b2c5605a55085f2cc17c69651fc6c8
Nathan Hannon <nathan@hannon.me>
2020-09-16 16:35:23 -0400

    updated ssl

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -4,7 +4,7 @@ import { Card, CardBody, CardTitle, CardImg, CardText } from 'reactstrap';
 
 export const ProjectCard = props => {
     return (
-        <Link to={"/Details/" + props.projects.project_id} className="text-dark">
+        <Link to={"/api/Details/" + props.projects.project_id} className="text-dark">
             <Card outline color="primary" >
                 <CardImg className='project-card' src={props.projects.image_url} width='320px' height='200px' />
                 <CardBody>
