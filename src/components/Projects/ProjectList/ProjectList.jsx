a83f73cb096c9329d3cf46a5bef5bac0cc08b050
Nathan Hannon <nathan@hannon.me>
2020-09-16 21:36:56 -0400

    added mobile view

--- src/components/Projects/ProjectList/ProjectList.jsx
+++ src/components/Projects/ProjectList/ProjectList.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import { ProjectCard } from '../ProjectCard';
 import { Container } from "reactstrap";
+import { BrowserView, MobileView } from "react-device-detect";
 import './ProjectList.css'
+import { Link } from "react-router-dom";
 
 //Creates the project list using the card component
 export const ProjectList = props => (
-<div>
-    <Container className="project-list">
-        {props.projects.map((projects) => <ProjectCard key={projects.project_id} projects={projects} />)}
-    </Container> 
-</div>
+    <div>
+        <Container className="project-list">
+            <BrowserView>
+                {props.projects.map((projects) => <ProjectCard key={projects.project_id} projects={projects} />)}
+            </BrowserView>
+            <MobileView>
+                {props.projects.map((projects) => <Link to={"/Details/" + props.projects.project_id} className="text-dark" />)}
+            </MobileView>
+        </Container>
+    </div>
 );
