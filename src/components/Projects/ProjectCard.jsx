9d1083cbd63f6dda779467e9c5c252b54f122242
Nathan Hannon <nathan@hannon.me>
2020-09-16 21:41:26 -0400

    added mobile view?

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 import { Card, CardBody, CardTitle, CardImg, CardText } from 'reactstrap';
+import { BrowserView, MobileView } from "react-device-detect";
 
 export const ProjectCard = props => {
     return (
-        <Link to={"/Details/" + props.projects.project_id} className="text-dark">
-            <Card outline color="primary" >
-                <CardImg className='project-card' src={props.projects.image_url} width='320px' height='200px' />
-                <CardBody>
-                    <CardTitle className="text-dark">{props.projects.name}</CardTitle>
-                    <CardText className="text-dark">{props.projects.description}</CardText>
-                </CardBody>
-            </Card>
-        </Link>
+        <div>
+            <BrowserView>
+                <Link to={"/Details/" + props.projects.project_id} className="text-dark">
+                    <Card outline color="primary" >
+                        <CardImg className='project-card' src={props.projects.image_url} width='320px' height='200px' />
+                        <CardBody>
+                            <CardTitle className="text-dark">{props.projects.name}</CardTitle>
+                            <CardText className="text-dark">{props.projects.description}</CardText>
+                        </CardBody>
+                    </Card>
+                </Link>
+            </BrowserView>
+            <MobileView>
+                <Link to={"/Details/" + props.projects.project_id} className="text-dark" />
+            </MobileView>
+        </div>
+
+
     );
 }
