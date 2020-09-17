ca9d26e299707dea9d0f358bd02641ea17553fc5
Nathan Hannon <nathan@hannon.me>
2020-09-16 21:54:36 -0400

    reverted mobile build changes

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -1,27 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 import { Card, CardBody, CardTitle, CardImg, CardText } from 'reactstrap';
-import { BrowserView, MobileView } from "react-device-detect";
 
 export const ProjectCard = props => {
     return (
-        <div>
-            <BrowserView>
-                <Link to={"/Details/" + props.projects.project_id} className="text-dark">
-                    <Card outline color="primary" >
-                        <CardImg className='project-card' src={props.projects.image_url} width='320px' height='200px' />
-                        <CardBody>
-                            <CardTitle className="text-dark">{props.projects.name}</CardTitle>
-                            <CardText className="text-dark">{props.projects.description}</CardText>
-                        </CardBody>
-                    </Card>
-                </Link>
-            </BrowserView>
-            <MobileView>
-                <Link to={"/Details/" + props.projects.project_id} className="text-dark" />
-            </MobileView>
-        </div>
-
-
+        <Link to={"/Details/" + props.projects.project_id} className="text-dark">
+            <Card outline color="primary" >
+                <CardImg className='project-card' src={props.projects.image_url} width='320px' height='200px' />
+                <CardBody>
+                    <CardTitle className="text-dark">{props.projects.name}</CardTitle>
+                    <CardText className="text-dark">{props.projects.description}</CardText>
+                </CardBody>
+            </Card>
+        </Link>
     );
 }
--- src/components/Projects/ProjectList/ProjectList.jsx
+++ src/components/Projects/ProjectList/ProjectList.jsx
@@ -1,20 +1,13 @@
 import React from "react";
 import { ProjectCard } from '../ProjectCard';
 import { Container } from "reactstrap";
-import { BrowserView, MobileView } from "react-device-detect";
 import './ProjectList.css'
-import { Link } from "react-router-dom";
 
 //Creates the project list using the card component
 export const ProjectList = props => (
     <div>
         <Container className="project-list">
-            <BrowserView>
-                {props.projects.map((projects) => <ProjectCard key={projects.project_id} projects={projects} />)}
-            </BrowserView>
-            <MobileView>
-                {props.projects.map((projects) => <Link to={"/Details/" + props.projects.project_id} className="text-dark" />)}
-            </MobileView>
+            {props.projects.map((projects) => <ProjectCard key={projects.project_id} projects={projects} />)}
         </Container>
     </div>
 );
