c12d24f0ae3793e1e5fa760c4dc745fe30c4bb20
Nathan Hannon <nathan@hannon.me>
2020-09-26 09:02:22 -0400

    temporarily reverted service worker change

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -140,11 +140,11 @@ export function unregister() {
   }
 }
 
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.5.0/workbox-sw.js');
+// importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.5.0/workbox-sw.js');
 
-if (workbox) {
-  console.log(`Yay! Workbox is loaded 🎉`);
-  workbox.precaching.precacheAndRoute([]);
-} else {
-  console.log(`Boo! Workbox didn't load 😬`);
-}
+// if (workbox) {
+//   console.log(`Yay! Workbox is loaded 🎉`);
+//   workbox.precaching.precacheAndRoute([]);
+// } else {
+//   console.log(`Boo! Workbox didn't load 😬`);
+// }
