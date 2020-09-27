4c9c74ee580168caf99c438351a41134d01a57c8
Nathan Hannon <nathan@hannon.me>
2020-09-27 15:39:25 -0400

    changed card path

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import { Card, CardBody, CardTitle, CardImg, CardText } from 'reactstrap';
 
+let { projectId } = useParams()
 export const ProjectCard = props => {
     return (
-        <Link to={"/Details/" + props.projects.project_id} className="text-dark">
+        <Link to={"/Details/:projectId"} className="text-dark">
             <Card outline color="primary">
                 <CardImg className='project-card' src={props.projects.image_url} width='320px' height='200px' />
                 <CardBody>
