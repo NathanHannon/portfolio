fba1317a0a10aa21040913c13505a790b8370d8d
Nathan Hannon <nathan@hannon.me>
2020-10-01 11:59:15 -0400

    set async for Buy Me A Coffee Widget

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
-import { Card, CardBody, CardTitle, CardImg, CardText } from 'reactstrap';
+import { Card, CardBody, CardTitle, CardImg } from 'reactstrap';
 
 export const ProjectCard = props => {
     return (
@@ -9,7 +9,6 @@ export const ProjectCard = props => {
                 <CardImg className='project-card' src={props.projects.image_url} width='320px' height='200px' />
                 <CardBody>
                     <CardTitle className="text-dark">{props.projects.name}</CardTitle>
-                    {/* <CardText className="text-dark">{props.projects.description}</CardText> */}
                 </CardBody>
             </Card>
         </Link>
