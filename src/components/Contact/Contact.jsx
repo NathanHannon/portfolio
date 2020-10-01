a6c9a2dda8ea4bb24d76077162072d0afaee6296
Nathan Hannon <nathan@hannon.me>
2020-09-30 23:50:46 -0400

    added shadows and changed background opacity.

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -15,7 +15,7 @@ class Contact extends Component {
                 </header>
                 <hr></hr>
                 <div class="iframe-container">
-                    <iframe seamless="seamless" frameborder="0" src={`https://storage.googleapis.com/portfolio-file-bucket/Nathan_Hannon_Resume.pdf`} />
+                    <iframe seamless="seamless" frameborder="0" src={`https://storage.googleapis.com/portfolio-file-bucket/Nathan_Hannon_Resume.pdf`} title="Resume" />
                 </div>
             </div>
         );
