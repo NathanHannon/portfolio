b9dd1f16dc362298d4c3db9060e462daee191064
Nathan Hannon <nathan@hannon.me>
2020-10-02 09:41:29 -0400

    installed simple react flex grid

--- src/components/Projects/ProjectList/ProjectList.jsx
+++ src/components/Projects/ProjectList/ProjectList.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { ProjectCard } from '../ProjectCard';
-import { Container } from "reactstrap";
+import "react-simple-flex-grid";
 import './ProjectList.css';
+import "react-simple-flex-grid";
 
 //Creates the project list using the card component
 export const ProjectList = props => (
-    <Container className="projectList">
-        {props.projects.map((projects) => <ProjectCard key={projects.project_id} projects={projects} />)}
-    </Container>
+    <Row gutter={40}>
+        {props.projects.map((projects) => <Col xs={{ span: 6 }} sm={{ span: 4 }} md={{ span: 3 }} lg={{ span: 2 }} xl={{ span: 1 }}>
+            <ProjectCard key={projects.project_id} projects={projects} />
+        </Col>)}
+    </Row>
 );
