085b90f0fa99e48d30b410d599e012077d4b9fd5
Nathan Hannon <nathan@hannon.me>
2020-10-02 09:59:48 -0400

    reverted to container

--- src/components/Projects/ProjectList/ProjectList.jsx
+++ src/components/Projects/ProjectList/ProjectList.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import { ProjectCard } from '../ProjectCard';
+import { Container } from "reactstrap";
 import './ProjectList.css';
-import { Row, Col } from "react-simple-flex-grid";
 
 //Creates the project list using the card component
 export const ProjectList = props => (
-    <Row gutter={40}>
-        {props.projects.map((projects) => <Col xs={{ span: 6 }} sm={{ span: 4 }} md={{ span: 3 }} lg={{ span: 2 }} xl={{ span: 1 }}>
-            <ProjectCard key={projects.project_id} projects={projects} />
-        </Col>)}
-    </Row>
+    <Container className="project-list" >
+        {props.projects.map((projects) => <ProjectCard key={projects.project_id} projects={projects} />)}
+    </Container>
 );
