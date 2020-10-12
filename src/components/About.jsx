55b1bd50f6ef56be25e0bf564d327264a195bcf6
Nathan Hannon <nathan@hannon.me>
2020-10-12 15:28:04 -0400

    added a picture and changed font to Ubuntu

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Container } from 'reactstrap';
+import programming from '../assets/programming.svg';
 
 class About extends Component {
     render() {
@@ -19,6 +20,7 @@ class About extends Component {
                     I am proficient with HTML5/CSS/JavaScript and React.js/Node.js, as well as C#, Python and Java. I am also proficient with various databases including
                     Microsoft SQL, Oracle SQL, MySQL, and PostgreSQL. I am currently teaching myself TypeScript, Rust, Dart/Flutter, and the concept of PWAs; to broaden my skillset.
                 </p>
+                <img src={programming} alt={programming} />
             </Container>
         );
     }
