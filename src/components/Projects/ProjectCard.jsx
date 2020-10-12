1536530ca0a8aae2f1dd0703c389e582af71c5db
Nathan Hannon <nathan@hannon.me>
2020-10-12 15:59:55 -0400

    changed font to IBM Plex

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -4,7 +4,7 @@ import { Card, CardBody, CardTitle, CardImg } from 'reactstrap';
 
 export const ProjectCard = props => {
     return (
-        <Link to={"/Details/" + props.projects.project_id} className="text-dark">
+        <Link to={"/Details/" + props.projects.project_id} className="text-dark" id="project-link">
             <Card outline className="projectCard">
                 <CardImg className='project-card-image' src={props.projects.image_url} width='300px' height='200px' />
                 <CardBody>
