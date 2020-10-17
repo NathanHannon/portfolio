c225fc0c0046f801296510cb27061dab63e43eef
Nathan Hannon <nathan@hannon.me>
2020-10-16 21:38:13 -0400

    added description

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
-import { Card, CardBody, CardTitle, CardImg } from 'reactstrap';
+import { Card, CardBody, CardTitle, CardImg, CardHeader } from 'reactstrap';
 
 export const ProjectCard = props => {
     return (
         <Link to={"/Details/" + props.projects.project_id} className="text-dark" id="project-link">
             <Card outline className="projectCard">
-                <CardImg className='project-card-image' src={props.projects.image_url} width='300px' height='200px' />
-                <CardBody>
+                <CardHeader>
+                    <CardImg className='project-card-image' src={props.projects.image_url} width='300px' height='200px' />
                     <CardTitle className="text-dark">{props.projects.name}</CardTitle>
+                </CardHeader>
+                <CardBody>
+                    <p className='description'>{props.projects.description}</p>
                 </CardBody>
             </Card>
         </Link>
