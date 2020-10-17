622fa2ef9b01476c7bf6a9d35c283de569300ac7
Nathan Hannon <nathan@hannon.me>
2020-10-16 22:44:11 -0400

    removed details page due to redundancy

--- src/components/Projects/ProjectCard/ProjectCard.jsx
+++ src/components/Projects/ProjectCard/ProjectCard.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 import { Card, CardBody, CardTitle, CardImg, CardText } from 'reactstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './ProjectCard.css';
 
 export const ProjectCard = props => {
     return (
-        <Link to={"/Details/" + props.projects.project_id} className="text-dark" id="project-link">
-            <Card outline className="projectCard">
-                <CardImg className='project-card-image' src={props.projects.image_url} width='300px' height='200px' />
-                <CardBody>
-                    <CardTitle className="text-dark">{props.projects.name}</CardTitle>
-                    <CardText className="text-dark">{props.projects.description}</CardText>
-                </CardBody>
-            </Card>
-        </Link>
+        // <Link to={"/Details/" + props.projects.project_id} className="text-dark" id="project-link">
+        <Card outline className="projectCard">
+            <CardImg className='project-card-image' src={props.projects.image_url} width='300px' height='200px' />
+            <CardBody>
+                <CardTitle className="text-dark">{props.projects.name}</CardTitle>
+                <CardText className="text-dark">{props.projects.description}</CardText>
+                <a className="githubLink" href={this.state.projectDetails.repo_url} target="_blank" rel="noreferrer noopener" title="GitHub Repository"><FontAwesomeIcon icon={['fab', 'github']} size='2x' /></a>
+            </CardBody>
+        </Card>
+        // </Link>
     );
 }
