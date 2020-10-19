e7bfbd51424b3d5741ce71e0035289779bd83ed6
Nathan Hannon <nathan@hannon.me>
2020-10-19 19:07:25 -0400

    adjusted mobile layout

--- src/components/Projects/ProjectCard/ProjectCard.jsx
+++ src/components/Projects/ProjectCard/ProjectCard.jsx
@@ -11,8 +11,10 @@ export const ProjectCard = props => {
             <CardImg className='projectCardImage' src={props.projects.image_url} width='300px' height='200px' />
             <CardBody>
                 <CardTitle className='text-dark' id='cardTitle'>{props.projects.name}</CardTitle>
-                <CardText className='text-dark'>{props.projects.description}</CardText>
-                <a className='githubLink' href={props.projects.repo_url} target='_blank' rel='noreferrer noopener' title='GitHub Repository'><FontAwesomeIcon icon={['fab', 'github']} size='2x' /></a>
+                <CardText className='text-dark'>{props.projects.description}
+                    <a className='githubLink' href={props.projects.repo_url} target='_blank' rel='noreferrer noopener' title='GitHub Repository'><FontAwesomeIcon icon={['fab', 'github']} size='2x' /></a>
+                </CardText>
+
             </CardBody>
         </Card>
         // </Link>
