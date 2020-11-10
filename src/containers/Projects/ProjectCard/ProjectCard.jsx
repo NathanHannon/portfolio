08e54ec718c4c8af497a0d466d62db4acedc6fe5
Nathan Hannon <nathan@hannon.me>
2020-11-10 17:52:09 -0500

    adjusted card height and fixed embedded link

--- src/containers/Projects/ProjectCard/ProjectCard.jsx
+++ src/containers/Projects/ProjectCard/ProjectCard.jsx
@@ -1,12 +1,12 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 import { Card, CardBody, CardImg, CardText, CardTitle } from 'reactstrap';
 import './ProjectCard.css';
 
 export const ProjectCard = props => {
     return (
-        <Link to={props.projects.repo_url} target='_blank' rel='noreferrer noopener' title='GitHub Repository'>
+        <a href={props.projects.repo_url} target='_blank' rel='noreferrer noopener' title='GitHub Repository'>
             <Card outline className='projectCard'>
                 <CardImg className='projectCardImage' src={props.projects.image_url} width='300px' height='200px' />
                 <CardBody>
@@ -16,7 +16,7 @@ export const ProjectCard = props => {
                     </CardText>
                 </CardBody>
             </Card>
-        </Link>
+        </a>
 
         // {'/details/' + props.projects.project_id} className='text-dark' id='project-link'
     );
