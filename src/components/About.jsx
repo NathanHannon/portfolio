74d59a675aca5b8dd3d6bb6446a14b725d61a0ab
Nathan Hannon <nathan@hannon.me>
2020-12-06 22:57:29 -0500

    fixed more typos and expanded acronyms

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,13 +10,13 @@ class About extends Component {
                 </header>
                 <h3><u>Interests</u></h3>
                 <p>
-                    I have a wide variety of interests including reading books (both fiction and non-fiction), fishing, golfing, video games, programming,and technology. I also try to keep up with the latest trends in the technology space, even if I won't learn them directly.
+                    I have a wide variety of interests including reading books (both fiction and non-fiction), fishing, golfing, video games, programming, and technology. I also try to keep up with the latest trends in the technology space, even if I won't learn them directly.
                     I believe it is good to have an idea of the direction and developments in the industry and those that affect it and which we affect in return.
                 </p>
                 <h3><u>Skills</u></h3>
                 <p>
                     I am proficient with HTML5/CSS/JavaScript and React.js/Node.js, as well as C#, Python and Java. I am also proficient with various databases including
-                    Microsoft SQL, Oracle SQL, MySQL, and PostgreSQL. I am currently teaching myself TypeScript, Rust, Dart/Flutter, and the concept of PWAs; to broaden my skillset.
+                    Microsoft SQL, Oracle SQL, MySQL, and PostgreSQL. I am currently teaching myself TypeScript, Rust, Dart/Flutter, and the concept of Progressive Web Apps; to broaden my skillset.
                 </p>
                 <img src={programming} alt={programming} className='programming-image' />
             </div>
