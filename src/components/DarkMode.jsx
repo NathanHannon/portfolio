844742be04003bef28fefe74b08fe74fa5d50904
Nathan Hannon <nathan@hannon.me>
2020-12-09 18:21:21 -0500

    disabled dark mode, until a solution is found

--- src/components/DarkMode.jsx
+++ src/components/DarkMode.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import DarkModeToggle from "react-dark-mode-toggle";
-import useDarkMode from 'use-dark-mode'
+// import useDarkMode from 'use-dark-mode'
 
 export default () => {
-    const darkMode = useDarkMode(false);
+    // const darkMode = useDarkMode(false);
     return (
         <DarkModeToggle
             className='switch'
-            onChange={darkMode.toggle}
-            checked={darkMode.value}
+            // onChange={darkMode.toggle}
+            // checked={darkMode.value}
             size={55}
         />
     );
