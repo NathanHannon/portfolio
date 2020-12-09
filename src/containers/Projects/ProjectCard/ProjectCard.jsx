f7e2e6715b8857afdbcccc547b9f14f6f5171fa4
Nathan Hannon <nathan@hannon.me>
2020-12-09 10:31:23 -0500

    removed tilt package

--- src/containers/Projects/ProjectCard/ProjectCard.jsx
+++ src/containers/Projects/ProjectCard/ProjectCard.jsx
@@ -2,25 +2,21 @@
 import React from 'react';
 // import { Link } from 'react-router-dom'
 import { Card, CardBody, CardImg, CardText, CardTitle } from 'reactstrap';
-import Tilt from 'react-vanilla-tilt'
 import './ProjectCard.css';
 
 export const ProjectCard = props => {
     return (
-        <Tilt options={{ glare: true }}>
-            <a href={props.projects.repo_url} target='_blank' rel='noreferrer noopener' title='GitHub Repository'>
-                <Card outline className='projectCard data-tilt'>
-                    <CardImg className='projectCardImage' src={props.projects.image_url} width='300px' height='200px' />
-                    <CardBody>
-                        <CardTitle className='text-dark' id='cardTitle'>{props.projects.name}</CardTitle>
-                        <CardText className='text-dark'>{props.projects.description}
-                            {/* <a className='githubLink' href={props.projects.repo_url} target='_blank' rel='noreferrer noopener' title='GitHub Repository'><FontAwesomeIcon icon={['fab', 'github']} size='2x' /></a> */}
-                        </CardText>
-                    </CardBody>
-                </Card>
-            </a>
-        </Tilt>
-
+        <a href={props.projects.repo_url} target='_blank' rel='noreferrer noopener' title='GitHub Repository'>
+            <Card outline className='projectCard data-tilt'>
+                <CardImg className='projectCardImage' src={props.projects.image_url} width='300px' height='200px' />
+                <CardBody>
+                    <CardTitle className='text-dark' id='cardTitle'>{props.projects.name}</CardTitle>
+                    <CardText className='text-dark'>{props.projects.description}
+                        {/* <a className='githubLink' href={props.projects.repo_url} target='_blank' rel='noreferrer noopener' title='GitHub Repository'><FontAwesomeIcon icon={['fab', 'github']} size='2x' /></a> */}
+                    </CardText>
+                </CardBody>
+            </Card>
+        </a>
         // {'/details/' + props.projects.project_id} className='text-dark' id='project-link'
     );
 }
