76529132623f3907e4921c3992537fdc68deee21
Nathan Hannon <nathan@hannon.me>
2020-12-09 18:22:18 -0500

    one last fix attempt

--- src/containers/Projects/ProjectCard/ProjectCard.jsx
+++ src/containers/Projects/ProjectCard/ProjectCard.jsx
@@ -6,19 +6,21 @@ import './ProjectCard.css';
 
 export const ProjectCard = props => {
     return (
-        <a href={props.projects.repo_url} target='_blank' rel='noreferrer noopener' title='GitHub Repository'>
-            <Card outline id='projectCard'>
-                <CardImg className='projectCardImage' src={props.projects.image_url} width='300px' height='200px' />
-                <CardBody>
-                    <body>
-                        <CardTitle id='cardTitle'>{props.projects.name}</CardTitle>
-                        <CardText className='text-dark'>{props.projects.description}
-                            {/* <a className='githubLink' href={props.projects.repo_url} target='_blank' rel='noreferrer noopener' title='GitHub Repository'><FontAwesomeIcon icon={['fab', 'github']} size='2x' /></a> */}
-                        </CardText>
-                    </body>
-                </CardBody>
-            </Card>
-        </a>
+        <body>
+            <a href={props.projects.repo_url} target='_blank' rel='noreferrer noopener' title='GitHub Repository'>
+                <Card outline id='projectCard'>
+                    <CardImg className='projectCardImage' src={props.projects.image_url} width='300px' height='200px' />
+                    <CardBody>
+                        <body>
+                            <CardTitle id='cardTitle'>{props.projects.name}</CardTitle>
+                            <CardText className='text-dark'>{props.projects.description}
+                                {/* <a className='githubLink' href={props.projects.repo_url} target='_blank' rel='noreferrer noopener' title='GitHub Repository'><FontAwesomeIcon icon={['fab', 'github']} size='2x' /></a> */}
+                            </CardText>
+                        </body>
+                    </CardBody>
+                </Card>
+            </a>
+        </body>
         // {'/details/' + props.projects.project_id} className='text-dark' id='project-link'
     );
 }
