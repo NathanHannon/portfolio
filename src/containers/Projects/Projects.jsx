495721479243631daa9c33f50c4572111d8f3499
Nathan Hannon <nathan@hannon.me>
2021-01-02 17:51:33 -0500

    changed url

--- src/containers/Projects/Projects.jsx
+++ src/containers/Projects/Projects.jsx
@@ -9,7 +9,8 @@ class Projects extends Component {
   };
 
   callAPI() {
-    const api_url = 'https://portfolio-2-bggv2.ondigitalocean.app/api/projects';
+    const api_url = 'https://nathanhannon.tech/api/projects'
+    // 'https://portfolio-2-bggv2.ondigitalocean.app/api/projects';
     fetch(api_url)
       .then(res => res.json())
       .then(data => this.setState({ projects: data }));
