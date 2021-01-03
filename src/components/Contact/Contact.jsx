80b0ff7dacc156a8a3d0eb888639a74cf698e9ec
Nathan Hannon <nathan@hannon.me>
2021-01-02 19:01:41 -0500

    added resume url

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -14,7 +14,7 @@ class Contact extends Component {
                     <a href='mailto:nathan@hannon.me' className='contactLink' alt='Send email to nathan@hannon.me'> Send an email!</a>
                 </header>
                 <div class='iframe-container'>
-                    <iframe seamless='seamless' frameborder='0' src={`https://storage.googleapis.com/portfolio-file-bucket/Nathan_Hannon_Resume.pdf`} title='Resume' />
+                    <iframe seamless='seamless' frameborder='0' src={'https://storage.googleapis.com/portfolio-file-bucket/Nathan_Hannon_Resume.pdf'} title='Resume' />
                 </div>
             </div>
         );
