5ad769d7ba6e2f82bcb413508257ec9e4cf1002e
Nathan Hannon <nathan@hannon.me>
2021-01-24 12:16:15 -0500

    added space to label

--- src/containers/Projects/ProjectCard/ProjectCard.jsx
+++ src/containers/Projects/ProjectCard/ProjectCard.jsx
@@ -6,7 +6,7 @@ import './ProjectCard.css';
 
 export const ProjectCard = props => {
     return (
-        <a href={props.projects.repo_url} target='_blank' rel='noreferrer noopener' title={props.projects.name + 'GitHub Repository'}>
+        <a href={props.projects.repo_url} target='_blank' rel='noreferrer noopener' title={props.projects.name + ' GitHub Page'}>
             <Card outline className='projectCard'>
                 <CardImg className='projectCardImage' src={props.projects.image_url} width='300px' height='200px' />
                 <CardBody>
