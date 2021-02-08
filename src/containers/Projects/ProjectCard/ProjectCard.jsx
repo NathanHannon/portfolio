f2a95bdd86b3de47d8e1043c7240e3239b406b70
Nathan Hannon <nathan@hannon.me>
2021-02-07 22:12:22 -0500

    undid changes

--- src/containers/Projects/ProjectCard/ProjectCard.jsx
+++ src/containers/Projects/ProjectCard/ProjectCard.jsx
@@ -8,7 +8,7 @@ export const ProjectCard = props => {
     return (
         <a href={props.projects.repo_url} target='_blank' rel='noreferrer noopener' title={props.projects.name + ' GitHub Page'}>
             <Card outline className='projectCard'>
-                <CardImg className='projectCardImage' src={props.projects.image_url} /*width='300px' height='200px'*/ className="img-fluid" />
+                <CardImg className='projectCardImage' src={props.projects.image_url} width='300px' height='200px' />
                 <CardBody>
                     <CardTitle className='text-dark' id='cardTitle'>{props.projects.name}</CardTitle>
                     <CardText className='text-dark'>{props.projects.description}
