3d16fe42e993ddd650d9e56d8eb4a2cc84afe135
Nathan Hannon <nathan@hannon.me>
2021-04-01 15:06:04 -0400

    Fixed grammatical error

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,13 +12,13 @@ class About extends Component {
                 <p><b>My name is Nathan Hannon and I am a Computer Programming/Analysis graduate from Conestoga College.</b></p>
                 <h3>Interests</h3>
                 <p>
-                    I have a wide variety of interests including reading books (both fiction and non-fiction), fishing, golfing, video games, programming, and technology. I also try to keep up with the latest trends in the technology space, even if I won't learn them directly.
+                    I have a wide variety of interests including reading books (both fiction and non-fiction), fishing, golfing, video games, programming, and technology. I also try to keep up with the latest trends in the technology space, even if I don't end up learn them directly.
                     I believe it is good to have an idea of the direction and developments in the industry and those that affect it and which we affect in return.
                 </p>
                 <h3>Skills</h3>
                 <p>
                     I am proficient with HTML5/CSS/JavaScript and React.js/Node.js, as well as C#, Python and Java. I am also proficient with various databases including
-                    Microsoft SQL, Oracle SQL, MySQL, and PostgreSQL. I am currently practicing my Go programming and the concept of Progressive Web Apps; to broaden my skillset.
+                    Microsoft SQL, Oracle PL/SQL, MySQL, and PostgreSQL. I am currently practicing my Go programming and the concept of Progressive Web Apps; to broaden my skill set.
                 </p>
                 {/* <img src={programming} alt={programming} className='programming-image' /> */}
                 <p>
