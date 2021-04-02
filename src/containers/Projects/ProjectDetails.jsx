d853901e13de6c86967010a120fd903553da159c
Nathan Hannon <nathan@hannon.me>
2021-04-02 13:59:50 -0400

    reverted changes

--- src/containers/Projects/ProjectDetails.jsx
+++ src/containers/Projects/ProjectDetails.jsx
@@ -12,14 +12,8 @@ class ProjectDetails extends Component {
     callAPI() {
         let project_id = this.props.location.pathname.split('/').pop();
         let top_url = 'https://nathanhannon.dev';
-        if (window.location.href == 'https://portfolio-2-bggv2.ondigitalocean.app') {
-            top_url = 'https://portfolio-2-bggv2.ondigitalocean.app';
-        }
-        if (window.location.href == 'https://www.nathanhannon.dev') {
-            top_url = 'https://www.nathanhannon.dev';
-        }
         // https://portfolio-2-bggv2.ondigitalocean.app  //Alt URL
-        let project_url = `${top_url}/api/projects/${project_id}`;
+        const project_url = `${top_url}/api/projects/${project_id}`;
         fetch(project_url)
             .then(res => res.json())
             .then(data => this.setState({ projectDetails: data[0] }))
--- src/containers/Projects/Projects.jsx
+++ src/containers/Projects/Projects.jsx
@@ -10,14 +10,8 @@ class Projects extends Component {
 
   callAPI() {
     let top_url = 'https://nathanhannon.dev';
-    if (window.location.href == 'https://portfolio-2-bggv2.ondigitalocean.app') {
-      top_url = 'https://portfolio-2-bggv2.ondigitalocean.app';
-    }
-    if (window.location.href == 'https://www.nathanhannon.dev') {
-      top_url = 'https://www.nathanhannon.dev';
-    }
     // https://portfolio-2-bggv2.ondigitalocean.app  //Alt URL
-    let api_url = `${top_url}/api/projects`;
+    const api_url = `${top_url}/api/projects`;
     fetch(api_url)
       .then(res => res.json())
       .then(data => this.setState({ projectArray: data }));
