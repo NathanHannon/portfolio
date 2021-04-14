e066bee93450b818f7f36acf674dee9cfa990440
Nathan Hannon <nathan@hannon.me>
2021-04-14 12:16:34 -0400

    turned off reject unauthorized

--- server/database_connection.js
+++ server/database_connection.js
@@ -3,7 +3,7 @@ const pool = new Pool({
 	connectionString: process.env.DATABASE_URL,
 	ssl: {
 		// Reject unauthorized connections
-		rejectUnauthorized: true,
+		rejectUnauthorized: false,
 
 		// Path to DigitalOcean cert
 		ca: process.env.CA_CERT
