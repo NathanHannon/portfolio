cf9359bc06c050e81ced7655e17428ba0416d245
Nathan Hannon <nathan@hannon.me>
2021-04-14 13:23:42 -0400

    removed notice

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -26,8 +26,8 @@ class About extends Component {
                     <a href='https://www.linkedin.com/in/nathan-hannon' className='linkedinLink' target='_blank' rel='noreferrer noopener' title='LinkedIn Profile'><FontAwesomeIcon icon={['fab', 'linkedin']} size='2x' /></a>
                     <a href='https://dev.to/nathanhannon' className='devLink' target='_blank' rel='noreferrer noopener' title='Dev.to Profile'><FontAwesomeIcon icon={['fab', 'dev']} size='2x' /></a>
                 </p>
-                <hr />
-                <p>NOTE: Due to issues beyond my control (server-side database connection errors), the project page is NOT working as intended. Please visit <a href="https://github.com/NathanHannon">my GitHub profile</a> to see my projects. I apologize for any inconvenience</p>
+                {/* <hr />
+                <p>NOTE: Due to issues beyond my control (server-side database connection errors), the project page is NOT working as intended. Please visit <a href="https://github.com/NathanHannon">my GitHub profile</a> to see my projects. I apologize for any inconvenience</p> */}
             </div>
         );
     }
