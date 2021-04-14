cadcd933928c574e0350d9158b9b80d6291a306a
Nathan Hannon <nathan@hannon.me>
2021-04-14 13:14:58 -0400

    reverted to CA_CERT environment variable

--- server/database_connection.js
+++ server/database_connection.js
@@ -1,16 +1,13 @@
 const Pool = require('pg/lib').Pool;
+
 const pool = new Pool({
-	user: process.env.USERNAME,
-	host: process.env.HOSTNAME,
-	database: process.env.DATABASE,
-	password: process.env.PASSWORD,
-	port: process.env.PORT,
+	connectionString: process.env.DATABASE_URL,
 	ssl: {
-		rejectUnauthorized: true,
-		ca:
-			process.env.NODE_ENV === PRODUCTION
-				? process.env.CA_CERT
-				: fs.readFileSync("ca_cert.crt").toString(),
+		// Reject unauthorized connections
+		rejectUnauthorized: false,
+
+		// Path to DigitalOcean cert
+		ca: process.env.CA_CERT
 	},
 })
 	.on('connect', () => {
