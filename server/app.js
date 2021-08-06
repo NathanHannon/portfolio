efc53d2ca659467bce47ffe346a90581224d04cc
Nathan Hannon <nathan@hannon.me>
2021-08-05 20:55:04 -0400

    reorganized some code

--- server/app.js
+++ server/app.js
@@ -1,20 +1,20 @@
-const express = require('../node_modules/express');
+const express = require('express');
 const path = require('path');
-const logger = require('../node_modules/morgan');
+const logger = require('morgan');
+
+const projectRouter = require("./routes/projects")
+
+// app express object
 const app = express();
 app.enable('trust proxy');
 
-//---------------Routers-----------------
-var projectRouter = require("./routes/projects")
-
-//use required packages
+// use required packages
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
-// app.listen(3000);
 
-//use create route api
+// routers
 app.use("/", projectRouter);
 
 module.exports = app;
