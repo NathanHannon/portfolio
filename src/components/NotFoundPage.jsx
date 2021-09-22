db681d2a1cd8e53a65f1ed6654c2626161ceb5f1
Nathan Hannon <nathan@hannon.me>
2021-09-21 20:47:28 -0400

    Fixed grammar

--- src/components/NotFoundPage.jsx
+++ src/components/NotFoundPage.jsx
@@ -7,8 +7,8 @@ class NotFoundPage extends Component {
         return (
             <Container className='notFoundPage'>
                 <h1>Oops! We couldn't find that page.</h1>
-                <h3>You can always <a href='/'>go back.</a></h3>
-                <img src={not_found_image} width='800' height='500'></img>
+                <h3>Don't worry, you can always <a href='/'>go back</a>.</h3>
+                <img src={not_found_image} width='auto' height='500'></img>
                 <br />
                 <span>
                     Photo by <a href="https://unsplash.com/@rojekilian?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Sarah Kilian</a> on <a href="https://unsplash.com/s/photos/404-error?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
