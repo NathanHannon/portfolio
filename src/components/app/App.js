ff473bec6fcefa57d21d7648085327c5258d18f2
Nathan Hannon <nathan@hannon.me>
2021-09-23 12:37:22 -0400

    consolidated components

---
+++ src/components/app/App.js
@@ -0,0 +1,43 @@
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { createBrowserHistory } from 'history';
+import React, { Component } from 'react';
+import { Route, Router, Switch } from 'react-router';
+import About from '../About';
+import Contact from '../Contact/Contact';
+import Layout from '../Layout/Layout';
+import NotFoundPage from '../NotFoundPage';
+import './App.css';
+import Projects from '../Projects/Projects';
+
+library.add(fab, fas);
+const history = createBrowserHistory();
+
+class App extends Component {
+	render() {
+		return (
+			<div>
+				<Router history={history}>
+					<Layout />
+					<div className='App'>
+						<Switch>
+							<Route exact path='/' component={About} />
+							<Route path='/about' component={About} />
+							<Route path='/projects' component={Projects} />
+							<Route path='/contact' component={Contact} />
+							<Route component={NotFoundPage} />
+						</Switch>
+						{/* <br />
+            <span className="reference">Photo by <a href="https://unsplash.com/@goumbik?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Lukas Blazek</a> on <a href="https://unsplash.com/s/visual/c2903a3e-a65b-4741-93eb-2e53358bbfec?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+            </span> */}
+					</div>
+
+				</Router>
+			</div>
+		);
+	}
+}
+
+export default App;
