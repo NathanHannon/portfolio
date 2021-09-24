2bfd47dbb8f718269ef42825cb1ec0d57ceb6a2a
Nathan Hannon <nathan@hannon.me>
2021-09-23 22:56:18 -0400

    removed CodeQL reported vulnerability

--- server/app.js
+++ server/app.js
@@ -18,8 +18,8 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", projectRouter);
 
 // fallback for serving main page.
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-});
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../public/index.html'));
+// });
 
 module.exports = app;
