2675350ab520a655d92ee83a2cea60d75f7f6d94
Nathan Hannon <nathan@hannon.me>
2021-09-23 20:47:31 -0400

    changed function name to add clarity

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -8,8 +8,10 @@ class Projects extends Component {
     projectArray: []
   };
 
-  // fetches the data from the projects API query and puts it in the projects array by changing the state
-  callAPI() {
+  /**
+   * fetches the data from the projects API query and puts it in the projects array by changing the state
+   */
+  fetchProjectData() {
     let top_url = 'https://nathanhannon.dev';
     // https://portfolio-2-bggv2.ondigitalocean.app  //Alt URL
     const api_url = `${top_url}/api/projects`;
@@ -17,9 +19,11 @@ class Projects extends Component {
       .then(res => res.json())
       .then(data => this.setState({ projectArray: data }));
   }
-  //pull data from the backend (database) after the component mounts
+  /**
+   * pull data from the backend (database) after the component mounts
+   */
   componentDidMount() {
-    this.callAPI();
+    this.fetchProjectData();
   }
 
   render() {
