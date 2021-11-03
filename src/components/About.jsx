919ecee09ad2783dbc3b2f0576a248672e00949b
Nathan Hannon <nathan@hannon.me>
2021-11-03 14:01:16 -0400

    Changed header from skills to technical expertise

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,11 +14,10 @@ class About extends Component {
                 <p>
                     I have a wide variety of interests including reading books (both fiction and non-fiction), fishing, golfing, video games, programming, and technology. I try to keep up with the latest trends in the technology space and I believe it is good to have an idea of the direction and developments in the industry.
                 </p>
-                <h3>Skills</h3>
+                <h3>Technical Expertise</h3>
                 <p>
                     I am skilled with HTML/CSS/JavaScript, TypeScript, React, and Node.js for web development; as well as C#, Java, Python, and Go for application development. I am also proficient with the various SQL implementations.
                 </p>
-                {/* <img src={programming} alt={programming} className='programming-image' /> */}
                 <p>
                     <a href='https://github.com/NathanHannon' className='githubLink' target='_blank' rel='noreferrer noopener' title='GitHub Profile' tabindex="-1" aria-disabled="true"><FontAwesomeIcon icon={['fab', 'github']} size='2x' /></a>
                     <a href='https://www.linkedin.com/in/nathan-hannon' className='linkedinLink' target='_blank' rel='noreferrer noopener' title='LinkedIn Profile' tabindex="-1" aria-disabled="true"><FontAwesomeIcon icon={['fab', 'linkedin']} size='2x' /></a>
