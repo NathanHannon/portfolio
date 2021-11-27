7280d13911fe6f3eebff00207f74ecc1a82018f4
Nathan Hannon <nathan@hannon.me>
2021-11-27 14:47:56 -0500

    fixed broken imports

--- src/components/NavMenu.jsx
+++ src/components/NavMenu.jsx
@@ -1,6 +1,6 @@
 
 import React, { Component } from 'react';
-import { Nav, Navbar, NavDropdown, Button } from 'react-bootstrap';
+import { Nav, Navbar } from 'react-bootstrap';
 import Logo from '../assets/nathan.png';
 
 class NavMenu extends Component {
@@ -21,11 +21,11 @@ class NavMenu extends Component {
 
 	render() {
 		return (
-			<Navbar bg='primary' variant='dark' fixed='top'>
+			<Navbar bg='primary' variant='dark' fixed='top' expand='sm'>
 				<Navbar.Brand href='/' className='mr-auto brand' tabindex="-1" aria-disabled="true" alt="Home Page"><img src={Logo} alt="Branding" /></Navbar.Brand>
-				<Navbar.Toggle onClick={this.toggleNavbar} className='mr-2' />
-				<Navbar.Collapse className='d-sm-inline-flex flex-sm-row-reverse' isOpen={!this.state.collapsed} navbar>
-					<Nav navbar>
+				<Navbar.Toggle onClick={this.toggleNavbar} aria-controls='responsive-navbar-nav' />
+				<Navbar.Collapse className='d-sm-inline-flex flex-sm-row-reverse' id='responsive-navbar-nav' isOpen={!this.state.collapsed} navbar>
+					<Nav>
 						<Nav.Link href='/' className='text-white nav-link' tabindex="-1" aria-disabled="true">About Me</Nav.Link>
 						<Nav.Link href='/projects' className='text-white nav-link' tabindex="-1" aria-disabled="true">My Projects</Nav.Link>
 						<Nav.Link href='/contact' className='text-white nav-link' tabindex="-1" aria-disabled="true">Resumé &amp; Contact Info</Nav.Link>
--- src/components/Projects/ProjectCard/ProjectCard.jsx
+++ src/components/Projects/ProjectCard/ProjectCard.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card } from 'bootstrap';
+import { Card } from 'react-bootstrap';
 import './ProjectCard.css';
 
 export const ProjectCard = props => {
