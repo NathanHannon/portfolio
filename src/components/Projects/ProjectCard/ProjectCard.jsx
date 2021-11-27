fd6f819828a58bfec0d2759c177222a95d9cb9d3
Nathan Hannon <nathan@hannon.me>
2021-11-27 15:10:14 -0500

    made card title text dark

--- src/components/Projects/ProjectCard/ProjectCard.jsx
+++ src/components/Projects/ProjectCard/ProjectCard.jsx
@@ -8,8 +8,8 @@ export const ProjectCard = props => {
             <Card outline className='projectCard'>
                 <Card.Img className='projectCardImage' variant='top' src={props.projects.image_url} width='300px' height='200px' />
                 <Card.Body>
-                    <Card.Title id='cardTitle'>{props.projects.name}</Card.Title>
-                    <Card.Text className='text-dark'>{props.projects.description}</Card.Text>
+                    <Card.Title className='cardTitle text-dark'>{props.projects.name}</Card.Title>
+                    <Card.Text className='cardContent text-dark'>{props.projects.description}</Card.Text>
                 </Card.Body>
             </Card>
         </a>
