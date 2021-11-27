88520978024cbdf2d25f0b937a0171dde98e1fc5
Nathan Hannon <nathan@hannon.me>
2021-11-27 15:01:22 -0500

    fixed dependency issue...again

--- src/components/Projects/ProjectList/ProjectList.jsx
+++ src/components/Projects/ProjectList/ProjectList.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container } from 'reactstrap';
+import { Container } from 'react-bootstrap';
 import { ProjectCard } from '../ProjectCard/ProjectCard';
 import './ProjectList.css';
 
