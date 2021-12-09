ae976a21a53643f8f962e0278ee204f9b9eb935a
Nathan Hannon <nathan@hannon.me>
2021-12-08 22:59:07 -0500

    added anchor class\

--- src/components/Projects/ProjectCard/ProjectCard.jsx
+++ src/components/Projects/ProjectCard/ProjectCard.jsx
@@ -4,7 +4,7 @@ import './ProjectCard.css';
 
 export const ProjectCard = props => {
     return (
-        <a href={props.projects.repo_url} target='_blank' rel='noreferrer noopener' title={props.projects.name + ' GitHub Page'}>
+        <a className='projectAnchor' href={props.projects.repo_url} target='_blank' rel='noreferrer noopener' title={props.projects.name + ' GitHub Page'}>
             <Card outline className='projectCard'>
                 <Card.Img className='projectCardImage' variant='top' src={props.projects.image_url} width='300px' height='200px' />
                 <Card.Body>
