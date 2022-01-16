2c869c03b6cd70c60030f40bf5018842ded4b97a
Nathan Hannon <nathan@hannon.me>
2022-01-16 12:41:53 -0500

    added more icons

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import About from './About';
