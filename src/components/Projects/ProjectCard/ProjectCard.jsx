2587107fa422c6ca9a51487c6602cb131cf996fc
Nathan Hannon <nathan@hannon.me>
2023-11-07 19:39:48 -0500

    Fix transform property in About and Contact components

--- src/components/Projects/ProjectCard/ProjectCard.jsx
+++ src/components/Projects/ProjectCard/ProjectCard.jsx
@@ -6,20 +6,20 @@ export const ProjectCard = (props) => {
     return (
         // This is the link to the GitHub page for the project
         // Project details are mapped from the project array
-        // <a href={props.projects.repo_url} target='_blank' rel='noreferrer noopener' title={props.projects.name + ' GitHub Page'}>
-        <Card outline className='projectCard'>
-            <Card.Img className='projectCardImage' variant='top' src={props.projects.image_url} width='300px' height='200px' />
-            <Card.Body>
-                <Card.Title className='cardTitle text-dark'>{props.projects.name}</Card.Title>
-                <Card.Text className='cardContent text-dark'>{props.projects.description}</Card.Text>
-                {/* TODO: Separate buttons for GitHub and live? */}
-                <div className='btn-group' role='group'>
-                    <Button href={props.projects.repo_url} className='github-url-button'>GitHub</Button>
+        <a href={props.projects.repo_url} target='_blank' rel='noreferrer noopener' title={props.projects.name + ' GitHub Page'}>
+            <Card outline className='projectCard'>
+                <Card.Img className='projectCardImage' variant='top' src={props.projects.image_url} width='300px' height='200px' />
+                <Card.Body>
+                    <Card.Title className='cardTitle text-dark'>{props.projects.name}</Card.Title>
+                    <Card.Text className='cardContent text-dark'>{props.projects.description}</Card.Text>
+                    {/* TODO: Separate buttons for GitHub and live? */}
+                    {/* <div className='btn-group' role='group'> */}
+                    {/* <Button href={props.projects.repo_url} className='github-url-button'>GitHub</Button> */}
                     {/* only show the demo button if the project has a demo_url */}
-                    {props.projects.demo_url && <Button href={props.projects.demo_url} className='demo-url-button'>Demo</Button>}
-                </div>
-            </Card.Body>
-        </Card>
-        // </a >
+                    {/* {props.projects.demo_url && <Button href={props.projects.demo_url} className='demo-url-button'>Demo</Button>} */}
+                    {/* </div> */}
+                </Card.Body>
+            </Card>
+        </a >
     );
 }
