7995d495f59d1f7322acde981105f98785eef89d
Nathan Hannon <nathan@hannon.me>
2024-09-15 17:16:21 -0400

    Refactor package.json and package-lock.json to include @prisma/client version 5.19.1

--- server/routes/projects.js
+++ server/routes/projects.js
@@ -1,44 +1,29 @@
 const express = require('express');
 const router = express.Router();
 const db_connection = require("../database_connection");
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient();
 
 // -----------------------------------------------------------------------
-//get all projects
-const allProjects = "SELECT * FROM project ORDER BY position ASC";
-router.get("/projects", (req, res, next) => {
+// Get all projects
+router.get("/projects", async (req, res, next) => {
 	res.setHeader("X-Frame-Options", "DENY");
 	res.setHeader("Content-Security-Policy", "frame-ancestors 'none'");
 	// cache control
 	res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
-	db_connection.query(allProjects, (error, results) => {
-		if (error) {
-			console.log('no projects found', error, res.status(404))
-			return res.status(404).json('no projects found');
-		}
-		else {
-			console.log(res.status(200));
-			return res.status(200).json(results.rows);
-		}
-	});
+
+	try {
+		const allProjects = await prisma.project.findMany({
+			orderBy: { position: 'asc' }
+		});
+		return res.status(200).json(allProjects);
+	} catch (error) {
+		console.error('No projects found', error);
+		return res.status(404).json('No projects found');
+	}
 });
-// --------------------Keep this around in case a solution for the details page bug is found---------------------------------------------
-// get a project by Id
-// const singleProject = "SELECT * FROM projects WHERE id=$1";
-// router.get("/projects/:id", (req, res, next) => {
-//   const id = parseInt(req.params.id);
-//   db_connection.query(
-//     singleProject,
-//     [id],
-//     (error, results) => {
-//       if (error) {
-//         console.log('project not found', error, res.status(404))
-//         //return an error message stating there are no projects
-//         return res.status(404).json('project not found');
-//       }
-//       //return a single project
-//       console.log(res.status(200));
-//       return res.status(200).json(results.rows);
-//     }
-//   );
-// });
+
+module.exports = router;
+
 module.exports = router;
