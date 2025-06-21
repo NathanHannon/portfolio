7aff5532e96d1a7bce768bb13f952a91f40f91c0
Nathan Hannon <nathan@hannon.me>
2025-06-21 09:53:53 -0400

    Update .gitignore to include certs directory and modify database connection to use SSL certificate

--- server/database_connection.js
+++ server/database_connection.js
@@ -1,10 +1,14 @@
 // Import Pool directly from 'pg'
 const { Pool } = require('pg');
+const fs = require('fs');
+
+const db_ca = fs.readFileSync('./certs/portfolio-db-ca.crt');
 
 const pool = new Pool({
 	connectionString: process.env.DATABASE_URL,
 	ssl: {
-		rejectUnauthorized: false
+		ca: db_ca,
+		// rejectUnauthorized: false
 	}
 });
 
